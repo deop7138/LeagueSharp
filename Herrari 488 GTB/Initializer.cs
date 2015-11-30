2b2aa57428bb9fd4a7d334dac0fd0839e76f8fc6
deop7138 <hyunsang638@gmail.com>
2015-11-30 18:00:32 +0900

    Release Herrari 488 GTB

---
+++ Herrari 488 GTB/Initializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using LeagueSharp;
+
+namespace Herrari_488_GTB
+{
+    class Initializer
+    {
+        internal static void Initialize()
+        {
+            Console.WriteLine("Herrari 488 GTB: HelloWorld!");
+
+            MenuProvider.initialize();
+
+            if (PluginLoader.LoadPlugin(ObjectManager.Player.ChampionName))
+            {
+                MenuProvider.Champion.Drawings.addItem(" ");
+            }
+
+            AutoQuit.Load();
+
+            Console.WriteLine("Herrari 488 GTB: Initialized.");
+        }
+    }
+}
---
+++ Herrari 488 GTB/PluginLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using LeagueSharp;
+
+namespace Herrari_488_GTB
+{
+    class PluginLoader
+    {
+        internal static bool LoadPlugin(string PluginName)
+        {
+
+            if (CanLoadPlugin(PluginName))
+            {
+                DynamicInitializer.NewInstance(Type.GetType("Herrari_488_GTB.Plugins." + ObjectManager.Player.ChampionName));
+                return true;
+            }
+
+            return false;
+        }
+
+        internal static bool CanLoadPlugin(string PluginName)
+        {
+            return Type.GetType("Herrari_488_GTB.Plugins." + ObjectManager.Player.ChampionName) != null;
+        }
+    }
+}
