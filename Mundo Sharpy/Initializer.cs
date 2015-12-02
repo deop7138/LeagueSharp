5c44d3dc58ab06e3bb73b8a33d372da1b9378d31
deop7138 <hyunsang638@gmail.com>
2015-12-02 18:48:13 +0900

    Mundo Sharpy Release

---
+++ Mundo Sharpy/Initializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using LeagueSharp;
+
+namespace Mundo_Sharpy
+{
+    class Initializer
+    {
+        internal static void Initialize()
+        {
+            Console.WriteLine("Mundo Sharpy: HelloWorld!");
+
+            MenuProvider.initialize();
+
+            if (PluginLoader.LoadPlugin(ObjectManager.Player.ChampionName))
+            {
+                MenuProvider.Champion.Drawings.addItem(" ");
+            }
+
+            AutoQuit.Load();
+
+            Console.WriteLine("Mundo Sharpy: Initialized.");
+        }
+    }
+}
---
+++ Mundo Sharpy/PluginLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using LeagueSharp;
+
+namespace Mundo_Sharpy
+{
+    class PluginLoader
+    {
+        internal static bool LoadPlugin(string PluginName)
+        {
+
+            if (CanLoadPlugin(PluginName))
+            {
+                DynamicInitializer.NewInstance(Type.GetType("Mundo_Sharpy.Plugins." + ObjectManager.Player.ChampionName));
+                return true;
+            }
+
+            return false;
+        }
+
+        internal static bool CanLoadPlugin(string PluginName)
+        {
+            return Type.GetType("Mundo_Sharpy.Plugins." + ObjectManager.Player.ChampionName) != null;
+        }
+    }
+}
